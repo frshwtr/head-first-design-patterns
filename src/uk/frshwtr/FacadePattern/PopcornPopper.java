510eb2058f7e96c1c776dd815ff9d62c1835057b
ben <ben@frshwtr.uk>
2021-02-18 13:39:50 +0000

    Adding missing facade pattern classes

---
+++ src/uk/frshwtr/FacadePattern/PopcornPopper.java
@@ -0,0 +1,16 @@
+package uk.frshwtr.FacadePattern;
+
+public class
+PopcornPopper {
+    public void on() {
+        System.out.println("Popcorn popper on");
+    }
+
+    public void pop() {
+        System.out.println("Popping popcorn");
+    }
+
+    public void off() {
+        System.out.println("Popper off");
+    }
+}
---
+++ src/uk/frshwtr/FacadePattern/TheaterLights.java
@@ -0,0 +1,11 @@
+package uk.frshwtr.FacadePattern;
+
+public class TheaterLights {
+    public void dim(int value) {
+        System.out.println("Dimmin lights to" + value);
+    }
+
+    public void on() {
+        System.out.println("Lights on");
+    }
+}
